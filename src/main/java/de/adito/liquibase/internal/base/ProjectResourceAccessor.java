610504951f91a3646fae706b0edd975c2825d307
Werner Glanzer <w.glanzer@adito.de>
2020-09-28 08:20:40 +0200

    ProjectResourceAccessor: Fixed Path for compatibility reasons

--- src/main/java/de/adito/liquibase/internal/base/ProjectResourceAccessor.java
+++ src/main/java/de/adito/liquibase/internal/base/ProjectResourceAccessor.java
@@ -1,7 +1,6 @@
 package de.adito.liquibase.internal.base;
 
 import de.adito.liquibase.internal.changelog.IChangelogProvider;
-import de.adito.liquibase.nb.LiquibaseFolderService;
 import liquibase.resource.FileSystemResourceAccessor;
 import org.apache.commons.io.FileUtils;
 import org.jetbrains.annotations.*;
@@ -9,7 +8,6 @@ import org.netbeans.api.project.*;
 import org.openide.filesystems.FileUtil;
 
 import java.io.*;
-import java.util.Optional;
 
 /**
  * Provides access to resources inside a project
@@ -50,7 +48,8 @@ class ProjectResourceAccessor extends FileSystemResourceAccessor
   }
 
   /**
-   * Returns all available roots for this resource accessor
+   * Returns all available roots for this resource accessor.
+   * !! We have to use the project as root, because of backwards compatibility !!
    *
    * @param pChangelogProvider Provider for the currently selected changelog
    * @return the files
@@ -63,27 +62,10 @@ class ProjectResourceAccessor extends FileSystemResourceAccessor
       return null;
 
     Project project = FileOwnerQuery.getOwner(changeLog.toURI());
-    String aliasName = pChangelogProvider.findAliasName();
-    if (project == null)
-      return null;
 
-    if (aliasName == null)
-      // If there is no alias available in the current context,
-      // then we should be aware of all aliasses in the project.
-      return LiquibaseFolderService.getInstance(project).observeLiquibaseFolder()
-          .map(pFolderOpt -> pFolderOpt
-              .map(FileUtil::toFile)
-              .map(File::listFiles))
-          .blockingFirst(Optional.empty())
-          .orElse(null);
-
-    // Alias is available
-    return LiquibaseFolderService.getInstance(project).observeLiquibaseFolderForAlias(aliasName)
-        .map(pFolderOpt -> pFolderOpt
-            .map(FileUtil::toFile)
-            .map(pFile -> new File[]{pFile}))
-        .blockingFirst(Optional.empty())
-        .orElse(null);
+    // !! We have to use the project as root, because of backwards compatibility !!
+    File projectDir = FileUtil.toFile(project.getProjectDirectory());
+    return new File[]{projectDir};
   }
 
 }
